5074865edd175b9871322f1b1939144a07c1e4d2
Matthias <matthias@refidim.fritz.box>
2018-02-04 00:18:57 +0100

    Add code to fix navigation-redux-bug #3355, see https://github.com/react-navigation/react-navigation/pull/3368

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import PropTypes from 'prop-types';
 import { addNavigationHelpers } from "react-navigation";
 import NavigationStack from "./navigationStack";
 
 class AppNavigation extends Component {
+	static propTypes = {
+	    dispatch: PropTypes.func.isRequired,
+	    navigationState: PropTypes.object.isRequired,
+	};
+
+	_actionEventSubscribers = new Set();
+
+ _addListener = (eventName, handler) => {
+	 eventName === 'action' && this._actionEventSubscribers.add(handler);
+	 return {
+		 remove: () => {
+			 this._actionEventSubscribers.delete(handler);
+		 },
+	 };
+ };
+
+ componentDidUpdate(lastProps) {
+	 const lastState = lastProps.navigationState;
+	 this._actionEventSubscribers.forEach(subscriber => {
+		 subscriber({
+			 lastState: lastProps.navigationState,
+			 state: this.props.navigationState,
+			 action: this.props.lastAction,
+		 });
+	 });
+}
+
   render() {
     const { navigationState, dispatch } = this.props;
     return (
       <NavigationStack
-        navigation={addNavigationHelpers({ dispatch, state: navigationState })}
+        navigation={addNavigationHelpers({ dispatch, state: navigationState, addListener: this._addListener })}
       />
     );
   }
@@ -16,7 +44,8 @@ class AppNavigation extends Component {
 
 const mapStateToProps = state => {
   return {
-    navigationState: state.NavigationReducer
+    navigationState: state.NavigationReducer,
+		lastAction: state.lastAction
   };
 };
 
--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,9 +2,14 @@ import { combineReducers } from "redux";
 import NavigationReducer from "./navigationReducer";
 import AuthentificationReducer from "./authentificationReducer";
 
+function lastAction(state = null, action) {
+  return action;
+}
+
 const AppReducer = combineReducers({
   NavigationReducer,
-  AuthentificationReducer
+  AuthentificationReducer,
+  lastAction
 });
 
 export default AppReducer;
